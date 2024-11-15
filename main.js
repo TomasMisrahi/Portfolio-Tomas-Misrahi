efb15e49694841407078167e0c0755e661a136dd
Tomas Misrahi <tomasmis13@hotmail.com>
2024-11-14 21:28:07 -0300

    Implementación de Media Queries y de textos-imagenes Responsive

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Funcionalidad para flecha hacia arrriba*/
+//Funcionalidad para flecha hacia arrriba
 
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 150) {
@@ -16,7 +16,79 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
     });
 });
 
-/*Funcionalidad de redirección al clickear las cards del form*/
+
+//MENU HEADER HAMBURGUESA
+//Funcionalidad del menu hamburguesa
+
+const menuHamburguesa = document.querySelector('.menu-hamburguesa');
+const textosHeader = document.querySelectorAll('.cv a, .header-1 nav ul');
+
+let isVisible = false;
+
+// Para que desaparezca <= 1100
+function cerrarMenuEnMobile() {
+    if (window.innerWidth <= 1100) {
+        textosHeader.forEach(th => {
+            th.style.display = 'none';
+        });
+        isVisible = false;
+    }
+}
+
+// Llamar a la función al cargar la página
+cerrarMenuEnMobile();
+
+menuHamburguesa.addEventListener('click', () => {
+    if (window.innerWidth <= 1100) {
+        isVisible = !isVisible;
+        textosHeader.forEach(th => {
+            th.style.display = isVisible ? 'flex' : 'none';
+        });
+    }
+});
+
+// Para que reaparezca > 1100
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1100) {
+        textosHeader.forEach(th => {
+            th.style.display = 'flex';
+        });
+        isVisible = false;
+    } else if (!isVisible) {
+        textosHeader.forEach(th => {
+            th.style.display = 'none';
+        });
+    }
+});
+
+
+//FILTRADO PORTFOLIO
+//Interacciones de botones y su filtrado
+
+var botonMostrarTodo = document.getElementById('boton-mostrar-todo');
+var botonDiseñoWeb = document.getElementById('boton-diseño-web');
+var diseñoWeb = document.getElementById('diseño-web');
+var botonDiseñoGrafico = document.getElementById('boton-diseño-grafico');
+var diseñoGrafico = document.getElementById('diseño-grafico');
+
+botonMostrarTodo.addEventListener('click', function () {
+    diseñoWeb.style.display = 'flex';
+    diseñoGrafico.style.display = 'flex';
+});
+
+botonDiseñoWeb.addEventListener('click', function () {
+    diseñoGrafico.style.display = 'none';
+    diseñoWeb.style.display = 'flex';
+});
+
+botonDiseñoGrafico.addEventListener('click', function () {
+    diseñoWeb.style.display = 'none';
+    diseñoGrafico.style.display = 'flex';
+});
+
+
+//FORMULARIO
+//Funcionalidad de redirección al clickear las cards del form
 
 function redirect(element) {
     var url = element.getAttribute('data-url');
@@ -58,29 +130,3 @@ document.addEventListener('click', function (event) {
         menuOpcionesForm.style.display = 'none';
     }
 });
-
-
-//interacciones de botones y su filtrado
-
-var botonMostrarTodo = document.getElementById('boton-mostrar-todo');
-var botonDiseñoWeb = document.getElementById('boton-diseño-web');
-var diseñoWeb = document.getElementById('diseño-web');
-var botonDiseñoGrafico = document.getElementById('boton-diseño-grafico');
-var diseñoGrafico = document.getElementById('diseño-grafico');
-
-botonMostrarTodo.addEventListener('click', function () {
-    diseñoWeb.style.display = 'flex';
-    diseñoGrafico.style.display = 'flex';
-});
-
-botonDiseñoWeb.addEventListener('click', function () {
-    diseñoGrafico.style.display = 'none';
-    diseñoWeb.style.display = 'flex';
-});
-
-botonDiseñoGrafico.addEventListener('click', function () {
-    diseñoWeb.style.display = 'none';
-    diseñoGrafico.style.display = 'flex';
-});
-
-
